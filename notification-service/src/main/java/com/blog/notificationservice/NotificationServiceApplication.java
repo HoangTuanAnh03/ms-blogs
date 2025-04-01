d6ddf7d639a6f6ee61b1d704deabf5fcd4c0b796
Tuấn Anh <138606608+HoangTuanAnh03@users.noreply.github.com>
2025-04-01 16:07:53 +0700

    style: application.yml (fetch config from github)

--- notification-service/src/main/java/com/blog/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/blog/notificationservice/NotificationServiceApplication.java
@@ -2,14 +2,12 @@ package com.blog.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
-public class NotificationApplication {
+public class NotificationServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApplication.class, args);
+        SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
 }
